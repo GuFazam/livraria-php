6b4bc788246f8c2fddcfeac3da8b046a0d1b3ef7
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-06-23 21:49:44 -0300

    .

--- livros/novo-salvar.php
+++ livros/novo-salvar.php
@@ -1,3 +1,17 @@
 <?php 
+include "../includes/conexao.php";
+
+$nome = $_POST['titulo'];
+$isbn = $_POST['isbn'];
+$autor = $_POST['autor'];
+$editora = $_POST['editora'];
+$ano_publicacao = $_POST['ano_publicacao'];
+$valor = $_POST['valor'];
+$foto = $_POST['foto'];
+
+$sql = "insert into tb_livros(titulo, isbn, autor, editora, ano_publicacao, valor, foto) values('$titulo', '$isbn', '$editora', '$ano_publicacao', '$valor', '$foto')";
+
+mysqli_query($conexao, $sql);
+mysqli_close($conexao);
 
 ?>
